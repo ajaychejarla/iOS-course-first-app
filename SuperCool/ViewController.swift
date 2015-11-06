e88f8e0688ae45da75e99c7e1f1cc99ff3ae7df1
Sravanthi <Nani@Sravanthis-MacBook-Air.local>
2015-11-05 19:50:38 -0600

    Add new project

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hiNaniLogo: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    @IBOutlet weak var meUImage: UIImageView!
+    @IBOutlet weak var btnMakeCool: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MakeMeCool(sender: AnyObject) {
+         hiNaniLogo.hidden = false
+        bgImage.hidden = false
+        meUImage.hidden = false
+        btnMakeCool.hidden=true
+    }
 
 }
 
